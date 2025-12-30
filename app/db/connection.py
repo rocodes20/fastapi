a121d12d9e127d65f5299d2fa75b75249713297f
ROHIT <rocodesforever@gmail.com>
2025-12-30 12:16:40 +0530

    password alteration

--- app/db/connection.py
+++ app/db/connection.py
@@ -4,7 +4,7 @@ def get_db():
     connection = pymysql.connect(
         host="localhost",
         user = "root",
-        password="Pass123!",
+        password="",
         database="sample",
         cursorclass= pymysql.cursors.DictCursor
     )
